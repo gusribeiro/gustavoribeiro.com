83894322c1bbbe3c23358e72c49e3bf9e8b2fbe5
Gustavo Ribeiro <dev@gustavoribeiro.com>
2024-09-30 20:36:15 -0300

    chore: redirect to linkedin profile

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://www.linkedin.com/in/gusribeiro/',
+        permanent: false
+      }
+    ]
+  }
+};
 
 export default nextConfig;
